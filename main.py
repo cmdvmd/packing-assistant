428a6423688b5a824c2992df7bac157f533c4fcd
cmdvmd <55916643+cmdvmd@users.noreply.github.com>
2021-07-10 10:25:52 -0400

    Start Bags Screen UI

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import StringProperty
 from kivy.core.window import Window
 
 
-class PackingApp(App):
+class Bag(ButtonBehavior, BoxLayout):
+    """
+    Widget to represent a bag on `BagsScreen`
+    """
+
+    qr = StringProperty(' ')
+    name = StringProperty(' ')
+    description = StringProperty(' ')
+
+
+class BagsScreen(Screen):
+    """
+    Screen to show available bags
+    """
+
+    def new_bag(self):
+        """
+        Add new bag to screen
+        """
+
+
+class PackerApp(MDApp):
+    """
+    App to be run
+    """
+
     def build(self):
         """
         Configure application
         """
 
         self.icon = 'icon.png'
-        self.title = 'Packing Assistant'
+        self.title = 'Packer'
+
         return ScreenManager()
 
 
-Window.clearcolor = 0.98, 0.98, 0.98, 1
-Window.size = 250, 450
+# Resize window
+Window.size = 275, 475
 
 # Run app
-app = PackingApp()
+app = PackerApp()
 app.run()
